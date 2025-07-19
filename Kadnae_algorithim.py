b09f0a5e0881cc4fd6fffc7e2825bef4b86ea80b
gulu864 <suva.kanta4u@gmail.com>
2025-07-19 11:37:31 +0530

    Add files via upload

---
+++ Kadnae_algorithim.py
@@ -0,0 +1,18 @@
+#Finding maximum subarray sum#
+def Maxsubarray(a,a_size):
+
+    max = -99999999999
+    cmax = 0
+
+    for i in range(0,a_size):
+        cmax = cmax + a[i]
+        if max < cmax:
+            max = cmax
+        
+        if cmax < 0:
+            cmax = 0
+
+    return max
+
+a = [1,2,3,-4,5,-22,-4,25,2,-9]
+print("Maximum subarray: ",Maxsubarray(a,len(a)))
